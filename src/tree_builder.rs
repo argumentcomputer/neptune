72fe7ab4007b4691cec53b555bf9ef41647d9316
neithanmo <neithanmo@gmail.com>
2021-04-30 22:52:41 -0600

    Add a more fine-grained usage of GPUs when building trees
    fix bus_id error and update dependency
    
    use our custom bellperson repo
    
    Add a comment to the build_next tree method
    
    Add an Incomplete tree error variant. indicating there are unexpected
    zero-padded columns/leaves
    
    fix an issue that was caused by not adding the data before building the
    tree
    
    add a check to verify that there are no zero-padded columns.
    
    add a finish_building_columns method
    
    use the new error variant
    
    add some tests
    
    add a new finish_adding_leaves trait method
    
    retain the original build_tree method
    
    rename build_tree_step to build_tree_batch
    
    add a check for number of leaves unless the finish_adding_leaves is
    called
    
    add some tests
    
    update toolchain
    
    1. adds a build_tree method to ColumnBuilder
    2. verifies the number of leaves/columns
    3. remove unnecessary boolean flag
    
    use uuid for gpu selection

--- src/tree_builder.rs
+++ src/tree_builder.rs
@@ -14,6 +14,8 @@ where
 {
     fn add_leaves(&mut self, leaves: &[Fr]) -> Result<(), Error>;
     fn add_final_leaves(&mut self, leaves: &[Fr]) -> Result<(Vec<Fr>, Vec<Fr>), Error>;
+    fn finish_adding_leaves(&mut self);
+    fn build_next(&mut self) -> Result<Option<(Vec<Fr>, Vec<Fr>)>, Error>;
 
     fn reset(&mut self);
 }
@@ -29,6 +31,9 @@ where
     tree_constants: PoseidonConstants<Bls12, TreeArity>,
     tree_batcher: Option<Batcher<TreeArity>>,
     rows_to_discard: usize,
+    row_start: usize,
+    row_end: usize,
+    tree_data: Vec<Fr>,
 }
 
 impl<TreeArity> TreeBuilderTrait<TreeArity> for TreeBuilder<TreeArity>
@@ -52,16 +57,25 @@ where
 
     fn add_final_leaves(&mut self, leaves: &[Fr]) -> Result<(Vec<Fr>, Vec<Fr>), Error> {
         self.add_leaves(leaves)?;
+        self.build_tree(self.rows_to_discard)
+    }
 
-        let res = self.build_tree(self.rows_to_discard);
-        self.reset();
+    fn finish_adding_leaves(&mut self) {
+        self.fill_index = self.leaf_count;
+    }
 
-        res
+    // give the option to build the tree without blocking the callers' thread
+    // should be called multiple times until either Some(tree) or an error is returned
+    fn build_next(&mut self) -> Result<Option<(Vec<Fr>, Vec<Fr>)>, Error> {
+        self.build_next_batch(self.rows_to_discard)
     }
 
     fn reset(&mut self) {
         self.fill_index = 0;
         self.data.iter_mut().for_each(|place| *place = Fr::zero());
+        self.tree_data.clear();
+        self.row_start = 0;
+        self.row_end = self.leaf_count;
     }
 }
 
@@ -93,6 +107,9 @@ where
         max_tree_batch_size: usize,
         rows_to_discard: usize,
     ) -> Result<Self, Error> {
+        let row_start = 0;
+        let row_end = leaf_count;
+
         let builder = Self {
             leaf_count,
             data: vec![Fr::zero(); leaf_count],
@@ -104,8 +121,10 @@ where
                 None
             },
             rows_to_discard,
+            row_start,
+            row_end,
+            tree_data: vec![],
         };
-
         // Cannot discard the base row or the root.
         assert!(rows_to_discard < builder.tree_height());
 
@@ -116,6 +135,7 @@ where
         Ok(builder)
     }
 
+    // this method will block the callers thread until the tree is built
     pub fn build_tree(&mut self, rows_to_discard: usize) -> Result<(Vec<Fr>, Vec<Fr>), Error> {
         let final_tree_size = self.tree_size(rows_to_discard);
         let intermediate_tree_size = self.tree_size(0) + self.leaf_count;
@@ -126,6 +146,8 @@ where
         tree_data[0..self.leaf_count].copy_from_slice(&self.data);
 
         let (mut start, mut end) = (0, arity);
+
+        self.check_number_of_leaves()?;
 
         match &mut self.tree_batcher {
             Some(batcher) => {
@@ -173,7 +195,78 @@ where
 
         let base_row = tree_data[..self.leaf_count].to_vec();
         let tree_to_keep = tree_data[tree_data.len() - final_tree_size..].to_vec();
+        self.reset();
         Ok((base_row, tree_to_keep))
+    }
+
+    // this function should be called multiple times
+    // until all batches are processed
+    fn build_next_batch(
+        &mut self,
+        rows_to_discard: usize,
+    ) -> Result<Option<(Vec<Fr>, Vec<Fr>)>, Error> {
+        let final_tree_size = self.tree_size(rows_to_discard);
+        let intermediate_tree_size = self.tree_size(0) + self.leaf_count;
+        let arity = TreeArity::to_usize();
+
+        if self.tree_data.is_empty() {
+            self.check_number_of_leaves()?;
+            self.tree_data.resize(intermediate_tree_size, Fr::zero());
+            self.tree_data[0..self.leaf_count].copy_from_slice(&self.data);
+        }
+
+        match &mut self.tree_batcher {
+            Some(batcher) => {
+                let max_batch_size = batcher.max_batch_size();
+
+                if self.row_end < intermediate_tree_size {
+                    let row_size = self.row_end - self.row_start;
+                    assert_eq!(0, row_size % arity);
+                    let new_row_size = row_size / arity;
+                    let (new_row_start, new_row_end) = (self.row_end, self.row_end + new_row_size);
+
+                    let mut total_hashed = 0;
+                    let mut batch_start = self.row_start;
+                    while total_hashed < new_row_size {
+                        let batch_end =
+                            usize::min(batch_start + (max_batch_size * arity), self.row_end);
+                        let batch_size = (batch_end - batch_start) / arity;
+                        let preimages =
+                            as_generic_arrays::<TreeArity>(&self.tree_data[batch_start..batch_end]);
+                        let hashed = batcher.hash(&preimages)?;
+
+                        #[allow(clippy::drop_ref)]
+                        drop(preimages); // make sure we don't reference tree_data anymore
+                        self.tree_data[new_row_start + total_hashed
+                            ..new_row_start + total_hashed + hashed.len()]
+                            .copy_from_slice(&hashed);
+                        total_hashed += batch_size;
+                        batch_start = batch_end;
+                    }
+
+                    self.row_start = new_row_start;
+                    self.row_end = new_row_end;
+                    return Ok(None);
+                }
+            }
+            None => {
+                let (mut start, mut end) = (0, arity);
+                for i in self.leaf_count..intermediate_tree_size {
+                    self.tree_data[i] = Poseidon::new_with_preimage(
+                        &self.tree_data[start..end],
+                        &self.tree_constants,
+                    )
+                    .hash();
+                    start += arity;
+                    end += arity;
+                }
+            }
+        }
+
+        let base_row = self.tree_data[..self.leaf_count].to_vec();
+        let tree_to_keep = self.tree_data[self.tree_data.len() - final_tree_size..].to_vec();
+        self.reset();
+        Ok(Some((base_row, tree_to_keep)))
     }
 
     /// `tree_size` returns the number of nodes in the tree to cache.
@@ -245,6 +338,14 @@ where
         // The last element computed is the root.
         Ok(element)
     }
+
+    fn check_number_of_leaves(&self) -> Result<(), Error> {
+        if self.fill_index != self.leaf_count {
+            Err(Error::IncompleteTree(self.fill_index, self.leaf_count))
+        } else {
+            Ok(())
+        }
+    }
 }
 
 #[cfg(all(any(feature = "gpu", feature = "opencl"), not(target_os = "macos")))]
@@ -261,11 +362,47 @@ mod tests {
         test_tree_builder_aux(None, 512, 32, 512, 512);
         test_tree_builder_aux(Some(BatcherType::CPU), 512, 32, 512, 512);
 
+        test_tree_builder_missing_leaves(Some(BatcherType::CPU), 512, 32, 512, 512);
+
         #[cfg(all(feature = "gpu", not(target_os = "macos")))]
         test_tree_builder_aux(Some(BatcherType::GPU), 512, 32, 512, 512);
 
+        #[cfg(all(feature = "gpu", not(target_os = "macos")))]
+        test_tree_builder_build_next(Some(BatcherType::CPU), 512, 32, 512, 512);
+
         #[cfg(all(feature = "opencl", not(target_os = "macos")))]
         test_tree_builder_aux(Some(BatcherType::OpenCL), 512, 32, 512, 512);
+
+        #[cfg(all(feature = "opencl", not(target_os = "macos")))]
+        test_tree_builder_build_next(Some(BatcherType::CPU), 512, 32, 512, 512);
+    }
+
+    fn create_leaves(
+        leaves: usize,
+        num_batches: usize,
+        max_leaf_batch_size: usize,
+    ) -> (Vec<Vec<Fr>>, Vec<Fr>) {
+        let batch_size = leaves / num_batches;
+
+        // Simplify computing the expected root.
+        let constant_element = Fr::zero();
+
+        let effective_batch_size = usize::min(batch_size, max_leaf_batch_size);
+
+        let mut total_leaves = 0;
+        let mut vec_leaves: Vec<Vec<Fr>> = vec![];
+        while total_leaves + effective_batch_size < leaves {
+            let leaves: Vec<Fr> = (0..effective_batch_size)
+                .map(|_| constant_element)
+                .collect();
+            total_leaves += leaves.len();
+            vec_leaves.push(leaves);
+        }
+
+        let final_leaves: Vec<_> = (0..leaves - total_leaves)
+            .map(|_| constant_element)
+            .collect();
+        (vec_leaves, final_leaves)
     }
 
     fn test_tree_builder_aux(
@@ -275,7 +412,7 @@ mod tests {
         max_leaf_batch_size: usize,
         max_tree_batch_size: usize,
     ) {
-        let batch_size = leaves / num_batches;
+        let constant_element = Fr::zero();
 
         for rows_to_discard in 0..3 {
             let mut builder = TreeBuilder::<U8>::new(
@@ -286,24 +423,12 @@ mod tests {
             )
             .unwrap();
 
-            // Simplify computing the expected root.
-            let constant_element = Fr::zero();
+            let (vec_leaves, final_leaves) =
+                create_leaves(leaves, num_batches, max_leaf_batch_size);
 
-            let effective_batch_size = usize::min(batch_size, max_leaf_batch_size);
-
-            let mut total_leaves = 0;
-            while total_leaves + effective_batch_size < leaves {
-                let leaves: Vec<Fr> = (0..effective_batch_size)
-                    .map(|_| constant_element)
-                    .collect();
-
-                let _ = builder.add_leaves(leaves.as_slice()).unwrap();
-                total_leaves += leaves.len();
-            }
-
-            let final_leaves: Vec<_> = (0..leaves - total_leaves)
-                .map(|_| constant_element)
-                .collect();
+            vec_leaves
+                .iter()
+                .for_each(|l| builder.add_leaves(l).unwrap());
 
             let (base, res) = builder.add_final_leaves(final_leaves.as_slice()).unwrap();
 
@@ -317,6 +442,86 @@ mod tests {
 
             assert_eq!(expected_size, res.len());
             assert_eq!(expected_root, computed_root);
+        }
+    }
+
+    fn test_tree_builder_build_next(
+        batcher_type: Option<BatcherType>,
+        leaves: usize,
+        num_batches: usize,
+        max_leaf_batch_size: usize,
+        max_tree_batch_size: usize,
+    ) {
+        for rows_to_discard in 0..3 {
+            let mut builder = TreeBuilder::<U8>::new(
+                batcher_type.clone(),
+                leaves,
+                max_tree_batch_size,
+                rows_to_discard,
+            )
+            .unwrap();
+
+            // Simplify computing the expected root.
+            let constant_element = Fr::zero();
+
+            let (vec_leaves, final_leaves) =
+                create_leaves(leaves, num_batches, max_leaf_batch_size);
+
+            vec_leaves
+                .iter()
+                .for_each(|l| builder.add_leaves(l).unwrap());
+
+            builder.add_leaves(final_leaves.as_slice()).unwrap();
+
+            let (base, res) = loop {
+                let res = builder.build_next().unwrap();
+                if res.is_some() {
+                    break res.unwrap();
+                }
+            };
+
+            let computed_root = res[res.len() - 1];
+
+            let expected_root = builder.compute_uniform_tree_root(final_leaves[0]).unwrap();
+            let expected_size = builder.tree_size(rows_to_discard);
+
+            assert_eq!(leaves, base.len());
+            assert!(base.iter().all(|x| *x == constant_element));
+
+            assert_eq!(expected_size, res.len());
+            assert_eq!(expected_root, computed_root);
+        }
+    }
+
+    fn test_tree_builder_missing_leaves(
+        batcher_type: Option<BatcherType>,
+        leaves: usize,
+        num_batches: usize,
+        max_leaf_batch_size: usize,
+        max_tree_batch_size: usize,
+    ) {
+        for rows_to_discard in 0..3 {
+            let mut builder = TreeBuilder::<U8>::new(
+                batcher_type.clone(),
+                leaves,
+                max_tree_batch_size,
+                rows_to_discard,
+            )
+            .unwrap();
+
+            let (vec_leaves, final_leaves) =
+                create_leaves(leaves, num_batches, max_leaf_batch_size);
+
+            vec_leaves.iter().for_each(|l| {
+                builder.add_leaves(l).unwrap();
+            });
+            builder.finish_adding_leaves();
+            assert!(builder.build_tree(rows_to_discard).is_ok());
+
+            builder.reset();
+
+            let res = builder.add_final_leaves(final_leaves.as_slice());
+            assert_eq!(Err(Error::IncompleteTree(final_leaves.len(), leaves)), res);
         }
     }
 }
--- src/triton/cl.rs
+++ src/triton/cl.rs
@@ -1,7 +1,8 @@
 use crate::error::{ClError, ClResult};
 use log::*;
-use rust_gpu_tools::opencl::{cl_device_id, Device, GPUSelector};
+use rust_gpu_tools::opencl::{cl_device_id, Device, DeviceUuid, GPUSelector};
 use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::fmt;
 use std::ptr;
 use std::sync::{Arc, Mutex, RwLock};
@@ -80,22 +81,15 @@ pub fn futhark_context(selector: GPUSelector) -> ClResult<Arc<Mutex<FutharkConte
 
 pub fn default_futhark_context() -> ClResult<Arc<Mutex<FutharkContext>>> {
     info!("getting default futhark context");
-    let bus_id = std::env::var("NEPTUNE_DEFAULT_GPU")
-        .ok()
-        .and_then(|v| match v.parse::<u32>() {
-            Ok(bus_id) => Some(bus_id),
-            Err(_) => {
-                error!("Bus-id '{}' is given in wrong format!", v);
-                None
-            }
-        });
+    let bus_id = std::env::var("NEPTUNE_DEFAULT_GPU").ok();
     match bus_id {
         Some(bus_id) => {
             info!(
-                "Using device with bus-id {} for creating the FutharkContext...",
+                "Using device with uuid {} for creating the FutharkContext...",
                 bus_id
             );
-            futhark_context(GPUSelector::BusId(bus_id))
+            let uuid = DeviceUuid::try_from(bus_id).map_err(|_| ClError::InvalidDeviceUuid)?;
+            futhark_context(GPUSelector::Uuid(uuid))
         }
         .or_else(|_| {
             error!(
