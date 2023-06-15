27cbbc77b94c7fadf05db5cefe2409c4a7d42119
Hanting Zhang <hantingz@usc.edu>
2023-06-14 17:11:36 -0700

    Unsafe Serialization for Poseidon Constants (#194)
    * abomonated poseidon constants
    
    * cargo fmt
    
    * clippy

--- src/hash_type.rs
+++ src/hash_type.rs
@@ -11,11 +11,14 @@
 /// `Strength` with `HashType` so that hashes with `Strength` other than `Standard` (currently only `Strengthened`)
 /// may still express the full range of hash function types.
 use crate::{Arity, Strength};
+use abomonation::Abomonation;
+use abomonation_derive::Abomonation;
 use ff::PrimeField;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Abomonation)]
 #[serde(bound(serialize = "F: Serialize", deserialize = "F: Deserialize<'de>"))]
+#[abomonation_omit_bounds]
 pub enum HashType<F: PrimeField, A: Arity<F>> {
     MerkleTree,
     MerkleTreeSparse(u64),
@@ -67,7 +70,8 @@ impl<F: PrimeField, A: Arity<F>> HashType<F, A> {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Abomonation)]
+#[abomonation_omit_bounds]
 pub enum CType<F: PrimeField, A: Arity<F>> {
     Arbitrary(u64),
     // See: https://github.com/bincode-org/bincode/issues/424
@@ -75,6 +79,7 @@ pub enum CType<F: PrimeField, A: Arity<F>> {
     // the generated code ends up being correct. But, in the future, do not
     // carelessly add new variants to this enum.
     #[serde(skip)]
+    #[abomonation_skip]
     _Phantom((F, A)),
 }
 
