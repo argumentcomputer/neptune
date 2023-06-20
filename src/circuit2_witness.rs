f54a333a43d4332492f99391c4b943ca22e03be5
François Garillot <francois@garillot.net>
2023-06-20 17:20:11 -0400

    refactor: replace `scalar_product` function with an inline fold operation

--- src/circuit2_witness.rs
+++ src/circuit2_witness.rs
@@ -235,7 +235,10 @@ where
 
                 // sparse mds
                 {
-                    elements_buffer[0] = scalar_product(elements, &m.w_hat);
+                    elements_buffer[0] = elements
+                        .iter()
+                        .zip(&m.w_hat)
+                        .fold(Scalar::ZERO, |acc, (&x, &y)| acc + (x * y));
 
                     for j in 1..width {
                         elements_buffer[j] = elements[j] + elements[0] * m.v_rest[j - 1];
@@ -314,12 +317,6 @@ where
 
         elements[1]
     }
-}
-
-fn scalar_product<Scalar: PrimeField>(a: &[Scalar], b: &[Scalar]) -> Scalar {
-    a.iter()
-        .zip(b)
-        .fold(Scalar::ZERO, |acc, (&x, &y)| acc + (x * y))
 }
 
 #[cfg(test)]
